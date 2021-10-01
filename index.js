b3cdb5966fd2ffc8de585d81ef4093508fb47f70
Anndrey Macedo <anndrey.macedo@sinqia.com.br>
2021-10-01 12:13:15 -0300

    Inclusão de comentários no código

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const express = require("express") // Criei uma váriavel para guardar a biblioteca que vou usar no projeto
+/**
+ * Entry point for the application.
+ */
 
+
+// Pacote de dependências
+const express = require("express") // Criei uma váriavel para guardar a biblioteca que vou usar no projeto
 require('dotenv').config()
 
+
+// Configuração do express
 const server = express()
 
-server.get('', (request,response) => { //Criação de rotas
+server.get('/', (request,response) => { //Criação de rotas
     return response.send(process.env.TESTE)
-    })
+})
 
 server.get('/usuario', (request,response) => { //Criação de rotas
-return response.send("Esse é os seus usuários")
+    return response.send("Esse é os seus usuários")
 })
 
 //Criação de rotas
-server.get('/clientes', (request,response) => {  
-
-    console.log(request.query.usuario)
+server.get('/clientes', (request,response) => {
     return response.send(request.query.usuario)
-    })
+})
 
-
-server.listen(8080) //Escolhendo a porta do servidor
+server.listen(8080, () =>{
+    console.log("Servidor rodando na porta 8080")
+} ) // Criação de porta
